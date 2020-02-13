ec33ed50f9729f693e306c1ec390a02a6e40c8e0
carlsachs <sachscarl@gmail.com>
2020-02-12 21:32:15 -0500

    committing more often and MvP:

--- src/Hooks/useLocalStorage.js
+++ src/Hooks/useLocalStorage.js
@@ -1,18 +1,18 @@
 import React, {useState} from "react";
 
 
-export default function useLocalStorage(key, initialValue) {
-
-    const setValue = value => {
-        setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
-      };
+export default function useLocalstorage(key, initialValue) {
 
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
 
-    return [storedValue];
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+      };
+
+    return [storedValue, setValue];
 
 }
